cbdaff9db69e67b25fca4733c60945ba75dd6867
iruta001 <81663508+iruta001@users.noreply.github.com>
2024-09-25 18:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def github_webhook():
                 "timestamp": datetime.utcnow()
             }
 
-    # Handle "Merge" event (optional brownie points)
+    # Handle "Merge" event 
     elif event_type == "pull_request" and payload['pull_request']['merged']:
         event_data = {
             "author": payload['pull_request']['user']['login'],
@@ -54,3 +54,9 @@ def github_webhook():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
